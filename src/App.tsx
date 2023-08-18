ed5d482e6f8cfe532b7ee5ccd40a71a4ca606314
stasfilippov <kron888999@gmail.com>
2023-08-18 23:53:27 +0300

    -add react-App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	console.log('App rendering')
+	// делает что-то полезное
+	// в конце обязательно возвращает JSX
+	return (
+		<div>
+			<PageTitle title={"This is app component"}/>
+			<PageTitle title={"My friend"}/>
+			<h3>Article 1</h3>
+			<Rating value={0}/>
+			<Accordion titleValue={'Title Accordion One'}/>
+			<Accordion titlevalue={'Title Accordion Two'}/>
+			<h3>Article 2</h3>
+			<Rating value={1}/>
+			<Rating value={2}/>
+			<Rating value={3}/>
+			<Rating value={4}/>
+			<Rating value={5}/>
+		</div>
+	);
 }
+
+function PageTitle(props: any) {
+	console.log('PageTitle rendering')
+	return (
+		<h1>{props.title}</h1>
+	)
+}
+
 
 export default App;
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Accordion(props: any) {
+	console.log('Accordion rendering')
+	return (
+		<div>
+			<AccordionTitle title={props.titleValue}/>
+			<AccordionBody/>
+		</div>
+	)
+}
+
+function AccordionTitle(props: any) {
+	console.log('AccordionTitle rendering')
+	return (
+		<h3>{props.title}</h3>
+	)
+}
+
+function AccordionBody() {
+	console.log('AccordionBody rendering')
+	return (
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+		</ul>
+	)
+}
+
+
+export default Accordion;
