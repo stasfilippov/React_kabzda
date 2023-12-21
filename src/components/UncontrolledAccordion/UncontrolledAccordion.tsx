b644fb1909489af4114b3e2e7d8162d73d68e1d3
stasfilippov <kron888999@gmail.com>
2023-12-21 22:48:06 +0300

    -add useReducer

--- src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -1,50 +1,67 @@
-import {useState} from 'react';
+import { useReducer, useState } from "react";
+import { action } from "@storybook/addon-actions";
 
 type AccordionPropsType = {
-	titleValue: string;
-}
+  titleValue: string;
+};
+
+type ActionType = {
+  type: string;
+};
+
+const reducer = (state: boolean, action: ActionType) => {
+  if (action.type === "TOGGLE-COLLAPSED") {
+    return !state;
+  }
+
+  return state;
+};
 
 export function UncontrolledAccordion(props: AccordionPropsType) {
+  // const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, dispatch] = useReducer(reducer, false);
 
-	const [collapsed, setCollapsed] = useState(false)
+  const onClickButtonHandler = () => {
+    dispatch({ type: "TOGGLE-COLLAPSED" });
+  };
 
-	const onClickButtonHandler = () => {
-		setCollapsed(!collapsed)
-	}
-
-	return (
-		<div>
-			<AccordionTitle title={props.titleValue} clickHandler={onClickButtonHandler}/>
-			{!collapsed && <AccordionBody/>}
-		</div>
-	)
+  return (
+    <div>
+      <AccordionTitle
+        title={props.titleValue}
+        clickHandler={onClickButtonHandler}
+      />
+      {!collapsed && <AccordionBody />}
+    </div>
+  );
 }
 
 type AccordionTitlePropsType = {
-	title: string;
-	clickHandler: () => void
-}
+  title: string;
+  clickHandler: () => void;
+};
 
 function AccordionTitle(props: AccordionTitlePropsType) {
-	console.log('AccordionTitle rendering')
+  console.log("AccordionTitle rendering");
 
-	const styleHeader = {
-		cursor: 'pointer'
-	}
+  const styleHeader = {
+    cursor: "pointer",
+  };
 
-	return (
-		<h3 style={styleHeader} onClick={props.clickHandler} >{props.title}</h3>
-	)
+  return (
+    <h3 style={styleHeader} onClick={props.clickHandler}>
+      {props.title}
+    </h3>
+  );
 }
 
 function AccordionBody() {
-	console.log('AccordionBody rendering')
-	return (
-		<ul>
-			<li>1</li>
-			<li>2</li>
-			<li>3</li>
-		</ul>
-	)
+  console.log("AccordionBody rendering");
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  );
 }
-
